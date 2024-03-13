8fd264557afb17377444c93a9719596079667fae
YooHaJin <yoohajin.dev@gmail.com>
2024-03-14 01:02:06 +0900

    feat: 경도, 위도, 배달비 제외한 데이터 생성

---
+++ DeliveryDataGenerator/data_generator.py
@@ -0,0 +1,29 @@
+import json
+import random
+import time
+
+import requests
+
+from confluent_kafka import SerializingProducer
+from datetime import datetime
+from faker import Faker
+from address import seoul_addresses
+
+faker = Faker()
+
+
+def generate_delivery_data():
+    return {
+        'deliveryId': faker.uuid4(),
+        'deliveryDate': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f%z'),
+        'userId': faker.simple_profile()['username'],
+        'foodCategory': random.choice(['한식', '중식', '양식', '일식', '아시안', '치킨', '버거', '분식']),
+        'foodPrice': round(random.randint(5000, 12000) * random.randint(1, 4), -1),
+        'paymentMethod': random.choice(['현금', '신용/체크카드', '네이버페이', '카카오페이', '애플페이', '토스페이']),
+        'deliveryDistance': round(random.uniform(0.2, 3.0), 1),
+        'deliveryDestination': random.choice(seoul_addresses),
+    }
+
+
+if __name__ == '__main__':
+    delivery = generate_delivery_data()
