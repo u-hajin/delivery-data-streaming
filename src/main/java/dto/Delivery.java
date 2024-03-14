8bc525fdfbb7efb25142bdcb0797076639dd5d31
YooHaJin <yoohajin.dev@gmail.com>
2024-03-14 23:22:02 +0900

    feat: delivery 데이터 객체 정의

---
+++ src/main/java/dto/Delivery.java
@@ -0,0 +1,20 @@
+package dto;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class Delivery {
+    private String deliveryId;
+    private Timestamp deliveryDate;
+    private String customerId;
+    private String foodCategory;
+    private double foodPrice;
+    private String paymentMethod;
+    private double deliveryDistance;
+    private String deliveryDestination;
+    private double destinationLat;
+    private double destinationLon;
+    private int deliveryCharge;
+}
