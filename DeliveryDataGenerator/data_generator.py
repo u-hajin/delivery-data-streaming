fc210217d8ddaa2bf20236ceaf14768578a44f64
YooHaJin <yoohajin.dev@gmail.com>
2024-05-13 02:28:35 +0900

    refactor: 위도, 경도, 배달비 데이터 생성 로직을 함수 안으로 삽입

--- DeliveryDataGenerator/data_generator.py
+++ DeliveryDataGenerator/data_generator.py
@@ -15,16 +15,23 @@ KST = timezone(timedelta(hours=9))
 
 
 def generate_delivery_data():
-    return {
-        'deliveryId': faker.uuid4(),
-        'deliveryDate': datetime.now(KST).strftime('%Y-%m-%dT%H:%M:%S.%f%z'),
-        'userId': faker.simple_profile()['username'],
-        'foodCategory': random.choice(['한식', '중식', '양식', '일식', '아시안', '치킨', '버거', '분식']),
-        'foodPrice': round(random.randint(5000, 12000) * random.randint(1, 4), -1),
-        'paymentMethod': random.choice(['현금', '신용/체크카드', '네이버페이', '카카오페이', '애플페이', '토스페이']),
-        'deliveryDistance': round(random.uniform(0.2, 3.0), 1),
-        'deliveryDestination': random.choice(seoul_addresses),
-    }
+    data = {'deliveryId': faker.uuid4(),
+            'deliveryDate': datetime.now(KST).strftime('%Y-%m-%dT%H:%M:%S.%f%z'),
+            'userId': faker.simple_profile()['username'],
+            'foodCategory': random.choice(['한식', '중식', '양식', '일식', '아시안', '치킨', '버거', '분식']),
+            'foodPrice': round(random.randint(5000, 12000) * random.randint(1, 4), -1),
+            'paymentMethod': random.choice(['현금', '신용/체크카드', '네이버페이', '카카오페이', '애플페이', '토스페이']),
+            'deliveryDistance': round(random.uniform(0.2, 3.0), 1),
+            'deliveryDestination': random.choice(seoul_addresses)}
+
+    kakaoApiKey = config['AUTHORIZATION']['KakaoApiKey']
+    location = get_location(data['deliveryDestination'], kakaoApiKey)['documents'][0]
+
+    data['destinationLat'] = location['y']
+    data['destinationLon'] = location['x']
+    data['deliveryCharge'] = round(random.randint(1000, 5000) + (data['deliveryDistance'] * 500), -1)
+
+    return data
 
 
 def get_location(address, apiKey):
@@ -53,24 +60,20 @@ if __name__ == '__main__':
 
     current_time = datetime.now()
 
-    while (datetime.now() - current_time).seconds < 200:
+    while (datetime.now() - current_time).seconds < 500:
         try:
-            delivery = generate_delivery_data()
-            kakaoApiKey = config['AUTHORIZATION']['KakaoApiKey']
-            location = get_location(delivery['deliveryDestination'], kakaoApiKey)['documents'][0]
-
-            delivery['destinationLat'] = location['y']
-            delivery['destinationLon'] = location['x']
-            delivery['deliveryCharge'] = round(random.randint(1000, 5000) + (delivery['deliveryDistance'] * 500), -1)
+            data = generate_delivery_data()
 
             producer.produce(
                 topic=topic,
-                key=delivery['deliveryId'],
-                value=json.dumps(delivery, ensure_ascii=False),
+                key=data['deliveryId'],
+                value=json.dumps(data, ensure_ascii=False),
                 on_delivery=delivery_report
             )
 
             producer.poll(0)
+
+            print(data)
 
             time.sleep(1)
         except Exception as e:
